1beb5e341484d92fd3cde37e2941b93fd4e963e0
A-Kujur-316 <86453222+A-Kujur-316@users.noreply.github.com>
2021-06-25 09:08:52 +0530

    Add files via upload

---
+++ Assets/racketMOVE.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class MoveRacket : MonoBehaviour
+{  
+	public float speed = 30;
+	void FixedUpdate()
+	{
+		float v = Input.GetAxisRaw("Vertical");
+		GetComponent<Rigidbody2D>().velocity = new Vector2(0, v) * speed;
+	}
+}
